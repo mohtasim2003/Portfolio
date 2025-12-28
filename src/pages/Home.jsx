68a339b2afe55db851998c8e7600f50159eb7a2c
MOHTASIM-UR-RAHMAN <novarahman2003@gmail.com>
2025-12-29 01:05:04 +0600

    pp updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import '../index.css'
 
 
 // 🖼️ Import Assets
-import photo from '../../public/./Me/pp.jpg'
+import photo from '../../public/./Me/pp.jpeg'
 import githubLogo from '../../public/github.png'
 import linkedinLogo from '../../public/linkedin.png'
 import gmailLogo from '../../public/gmail.png'
